044d6b8816560da77df5a583e08d1a602b5e642e
Ashley <a_falsario@yahoo.com>
2014-06-26 16:34:35 -0500

    Initial commit of resume and portfolio views with the layout master

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Laravel Blog</title>
+    @yield('topcontent')
+</head>
+<body>
+    @yield('content')
+
+    @yield('bottomcontent')
+</body>
+</html>
