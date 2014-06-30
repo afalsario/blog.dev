8e949cc1ddc78212ec4d4a1cf637b445e1597f93
Ashley <a_falsario@yahoo.com>
2014-06-30 14:34:02 -0500

    'added a custom error view for missing pages'

---
+++ app/views/errors/missing.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="container">
+    <h2 class="title">Oops!</h2>
+       <p class="tagline"> Something totally bad just happend. </p>
+</div>
+
+@stop
