b13ab1deabf78b0bdaf642a36e7e799f185d8824
Ashley <a_falsario@yahoo.com>
2014-06-30 14:40:47 -0500

    updated routes to use controllers

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -17,7 +17,27 @@ class HomeController extends BaseController {
 
 	public function showWelcome()
 	{
-		return View::make('hello');
+		return Redirect::action('HomeController@sayHello', ['Codeup']);
+	}
+
+	public function home(){
+		return View::make('home');
+	}
+
+	public function resume(){
+		return View::make('resume');
+	}
+
+	public function portfolio(){
+		return View::make('portfolio');
+	}
+
+	public function sayHello($name)
+	{
+        $data = array(
+        	'name' => $name
+        );
+        return View::make('temp.my-first-view')->with($data);
 	}
 
 }
