6c353cc6a05d1fc72d8fadc56ec7763873d54767
Ashley <a_falsario@yahoo.com>
2014-07-03 13:53:55 -0500

    modified date format and added a delete button

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -4,7 +4,13 @@
 
 
 {{{ $post->title }}}
+<br>
+{{{ $post->created_at->format('l, F jS Y @ h:i A') }}}
+<hr>
 {{{ $post->body }}}
 
+{{ Form::open(array('action' => array('PostsController@destroy', $post->id), 'method' => 'DELETE')) }}
+    {{ Form::submit('Delete') }}
+{{ Form::close() }}
 
 @stop
