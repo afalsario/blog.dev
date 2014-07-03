47c63bc2ff6dfb9b42016bf24b6886c44bdfe309
Ashley <a_falsario@yahoo.com>
2014-07-03 13:52:07 -0500

    added search field, modified date format, and added edit button

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -1,11 +1,30 @@
 @extends('layouts.master')
 
 @section('content')
+    <div class="container">
+        <h2>Blog Posts</h2>
+        <table>
+            <tr>
+                <th>Title</th>
+                <th>Creation Date</th>
+            </tr>
 
-@foreach ($posts as $post)
-{{{ $post->title }}}
-<hr>
-{{{ $post->body }}}
-@endforeach
+            @foreach ($posts as $post)
+                <tr>
+                    <td>{{{ $post->title }}} </td>
+                    <td>{{{ $post->created_at->format('l jS \of F Y h:i:s A') }}}</td>
+                    <td>
+                        <a href="{{ action('PostsController@edit', $post->id) }}"><button>Edit</button></a>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+        {{ $posts->links() }}
 
+        {{ Form::open(array('action' => 'PostsController@index', 'method' => 'get')) }}
+        {{ Form::text('search')}}
+        <button type="submit">Submit</button>
+        {{ Form::close() }}
+
+    </div>
 @stop
