ef870c253f82de6698fd6c1cbb45b3d2694a957c
Ashley <a_falsario@yahoo.com>
2014-07-03 13:46:34 -0500

    created a BaseModel that extends eloquent and changed other models to extend basemodel

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,7 +1,13 @@
 <?php
 
-class Post extends Eloquent {
+class Post extends BaseModel {
 
     protected $table = 'posts';
+
+    //Ensure data is valid based on type and size specifications
+    public static $rules = [
+        'title' => 'required|max:100',
+        'body' => 'required|max:10000'
+    ];
 
 }
