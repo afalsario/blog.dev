0a6c094b902f27e34abf163cc6445eb4cd359fb9
Ashley <a_falsario@yahoo.com>
2014-07-07 15:54:22 -0500

    created new blade for logging in

---
+++ app/views/login.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.blog-master')
+
+@section('topscript')
+<link href="/css/journal-bootstrap.min.css" rel="stylesheet" type="text/css">
+@stop
+
+@section('content')
+<div class="container">
+                <h5 class="text">Email</h5>
+                <div>
+                    {{ Form::open(array('action' => 'HomeController@doLogin'))}}
+                    {{ Form::text('email') }}
+                </div>
+                <h5 class="text">Password</h5>
+                <div>
+                    {{ Form::password('password') }}
+                    <div class="checkbox">
+                    <label>
+                        <input type="checkbox"> Checkbox
+                    </label>
+                    </div>
+                </div>
+            <div class="form-group">
+                {{ Form::submit('Log In', ['class' => 'btn btn-primary']) }}
+            </div>
+            {{ Form::close() }}
+</div>
+@stop
