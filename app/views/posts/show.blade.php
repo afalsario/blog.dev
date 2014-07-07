915615e36ad222d4efb31c987196252f4fe51d75
Ashley <a_falsario@yahoo.com>
2014-07-07 15:47:23 -0500

    working on css

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -1,16 +1,27 @@
-@extends('layouts.master')
+@extends('layouts.blog-master')
+
+@section('topscript')
+<link href="/css/journal-bootstrap.min.css" rel="stylesheet" type="text/css">
+@stop
 
 @section('content')
 
+<div class="container">
+    <h2>{{{ $post->title }}}</h2>
+    <h5 class="text">Author: {{{ $post->user->email }}}</h5>
+    <h5 class="text">{{{ $post->created_at->format('l, F jS Y @ h:i A') }}}</h5>
+    <hr>
+    <p class="lead"> {{{ $post->body }}} </p>
 
-{{{ $post->title }}}
+    @if ($post->img_path)
+
+    <img src="{{{ $post->img_path }}}" class="img-responsive">
+    @endif
 <br>
-{{{ $post->created_at->format('l, F jS Y @ h:i A') }}}
-<hr>
-{{{ $post->body }}}
+    <a href="{{ action('PostsController@edit', $post->id) }}"><button type="button" class="btn btn-link">Edit</button></a>
+    <button type="button" class="btn btn-link">{{ Form::open(array('action' => array('PostsController@destroy', $post->id), 'method' => 'DELETE')) }}
+        {{ Form::submit('Delete') }}</button>
+    {{ Form::close() }}
 
-{{ Form::open(array('action' => array('PostsController@destroy', $post->id), 'method' => 'DELETE')) }}
-    {{ Form::submit('Delete') }}
-{{ Form::close() }}
-
+</div>
 @stop
