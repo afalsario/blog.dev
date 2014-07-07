fb42f269ebdaedf3c8eea11b07c9e0b1b502cc01
Ashley <a_falsario@yahoo.com>
2014-07-07 15:43:11 -0500

    added the ability to log in and out

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,34 +2,70 @@
 
 class HomeController extends BaseController {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Default Home Controller
+    |--------------------------------------------------------------------------
+    |
+    | You may wish to use controllers instead of, or in addition to, Closure
+    | based routes. That's great! Here is an example controller method to
+    | get you started. To route to this controller, just add the route:
+    |
+    |   Route::get('/', 'HomeController@showWelcome');
+    |
+    */
 
-	public function showWelcome()
-	{
-		return Redirect::action('HomeController@sayHello', ['Codeup']);
-	}
+    public function showWelcome()
+    {
+        return Redirect::action('HomeController@sayHello', ['Codeup']);
+    }
 
-	public function home(){
-		return View::make('home');
-	}
+    public function home(){
+        return View::make('home');
+    }
 
-	public function sayHello($name)
-	{
+    public function sayHello($name)
+    {
         $data = array(
-        	'name' => $name
+            'name' => $name
         );
         return View::make('temp.my-first-view')->with($data);
-	}
+    }
+
+    public function login()
+    {
+        return View::make('login');
+    }
+
+    public function doLogin()
+    {
+        $email = Input::get('email');
+        $password = Input::get('password');
+
+        if(Auth::attempt(array('email' => $email, 'password' => $password), true))
+        {
+            Session::flash('infoMessage', 'You have logged in successfully.');
+            return Redirect::intended(action('PostsController@index'));
+        }
+        else
+        {
+            Session::flash('errorMessage', "Email or Password is incorrect");
+            // return Redirect::action('HomeController@login');
+            return Redirect::back();
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return Redirect::action('PostsController@index');
+    }
+
+
 
 }
+
+
+
+
+
