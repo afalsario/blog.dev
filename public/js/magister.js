644e48e3c27fc481f4e149bf2d8cb17643780345
Ashley <a_falsario@yahoo.com>
2014-07-07 15:53:00 -0500

    fixed blog link

--- public/js/magister.js
+++ public/js/magister.js
@@ -7,20 +7,27 @@ var section_show_time = 1000;
 
 // jQuery stuff
 jQuery(document).ready(function($) {
-
+	console.log('Document Loaded.');
 	// Switch section
-	$("a", '.mainmenu').click(function() 
+	$('a', '.mainmenu').click(function() 
 	{
-		if( ! $(this).hasClass('active') ) { 
+		// var test = $(this).hasClass('dpdn');
+		// console.log(test);
+
+		if( ! $(this).hasClass('active') && ! $(this).hasClass('dpdn')) {
 			current_item = this;
 			// close all visible divs with the class of .section
-			$('.section:visible').fadeOut( section_hide_time, function() { 
-				$('a', '.mainmenu').removeClass( 'active' );  
+			$('.section:visible').fadeOut( section_hide_time, function() {
+				$('a', '.mainmenu').removeClass( 'active' );
 				$(current_item).addClass( 'active' );
 				var new_section = $( $(current_item).attr('href') );
 				new_section.fadeIn( section_show_time );
 			} );
 		}
+		else
+		{
+			window.location.href = $(this).attr('href');
+		}
 		return false;
-	});		
+	});
 });
