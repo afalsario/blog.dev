9b6309be5749c0a9a906142779dccfca33550f4c
Ashley <a_falsario@yahoo.com>
2014-07-07 15:49:59 -0500

    adding upload image function

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,10 +4,29 @@ class Post extends BaseModel {
 
     protected $table = 'posts';
 
+    protected $imgDir = 'img-upload';
+
     //Ensure data is valid based on type and size specifications
     public static $rules = [
         'title' => 'required|max:100',
-        'body' => 'required|max:10000'
+        'body' => 'required'
     ];
 
+    public function posts()
+    {
+        return $this->hasMany('Post');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('User');
+    }
+
+    public function addUpLoadedImage($image)
+    {
+        $systemPath = public_path() . '/' . $this->imgDir . '/';
+        $imageName = $this->id . '-' . $image->getClientOriginalName();
+        $image->move($systemPath, $imageName);
+        $this->img_path = '/' . $this->imgDir . '/' . $imageName;
+    }
 }
