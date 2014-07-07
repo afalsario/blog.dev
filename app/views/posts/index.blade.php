147e45954c1a849543b9c84d4f6a1d2dc54d36b2
Ashley <a_falsario@yahoo.com>
2014-07-07 15:46:57 -0500

    working on nav bar and updating css

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -1,30 +1,43 @@
-@extends('layouts.master')
+@extends('layouts.blog-master')
+
+@section('topscript')
+<link href="/css/journal-bootstrap.min.css" rel="stylesheet" type="text/css">
+@stop
 
 @section('content')
+
+<form class="navbar-form navbar-left" role="search">
+  <div class="form-group">
+    {{ Form::open(array('action' => 'PostsController@index', 'method' => 'get')) }}
+        {{ Form::text('search')}}
+        {{ Form::submit('Search', ['class' => 'btn btn-primary btn-sm']) }}
+        {{ Form::close() }}
+  </div>
+</form>
+
+
     <div class="container">
         <h2>Blog Posts</h2>
-        <table>
+        <table class="table" style="margin:20">
             <tr>
                 <th>Title</th>
                 <th>Creation Date</th>
+                <th></th>
             </tr>
 
             @foreach ($posts as $post)
                 <tr>
-                    <td>{{{ $post->title }}} </td>
+                    <td class="lead">{{{ $post->title }}} </td>
                     <td>{{{ $post->created_at->format('l jS \of F Y h:i:s A') }}}</td>
                     <td>
-                        <a href="{{ action('PostsController@edit', $post->id) }}"><button>Edit</button></a>
+                        <a href="{{ action('PostsController@show', $post->id) }}"><button class="btn btn-primary">View</button></a>
                     </td>
                 </tr>
             @endforeach
         </table>
         {{ $posts->links() }}
 
-        {{ Form::open(array('action' => 'PostsController@index', 'method' => 'get')) }}
-        {{ Form::text('search')}}
-        <button type="submit">Submit</button>
-        {{ Form::close() }}
+        
 
     </div>
 @stop
