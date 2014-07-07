06586234e1a1e7a1ccca8fa7a835423b061e39b6
Ashley <a_falsario@yahoo.com>
2014-07-07 15:51:06 -0500

    created to separate layouts for the home page and the blog section

--- app/views/layouts/blog-master.blade.php
+++ app/views/layouts/blog-master.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport"    content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
 
     <title>Ashley Falsario</title>
 
@@ -12,10 +11,10 @@
 
     <!-- Bootstrap itself -->
     <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="/css/journal-bootstrap.min.css" rel="stylesheet" type="text/css">
 
     <!-- Custom styles -->
     <link rel="stylesheet" href="/css/magister.css">
+    <link href="/css/journal-bootstrap.min.css" rel="stylesheet" type="text/css">
 
     <!-- Fonts -->
     <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -23,17 +22,26 @@
     @yield('topscript')
 </head>
 
-<body class="theme-invert">
+@if(Auth::check())
+    <div class="user">Logged in as <span class="text-primary">{{ Auth::user()->email }}</span></div>
+    {{ link_to_action('PostsController@create', 'Create Post')}}
+    {{ link_to_action('HomeController@logout', 'Logout') }}
+@else
+    {{ link_to_action('HomeController@login', 'Login') }}
+@endif
 
+<body class="theme-invert">
+    <div class="container">
+        @if(Session::has('successMessage'))
+            <div class="alert alert-success">{{{ Session::get('successMessage') }}}</div>
+        @endif
+        @if(Session::has('errorMessage'))
+            <div class="alert alert-danger">{{{ Session::get('errorMessage') }}}</div>
+        @endif
+    </div>
     @yield('content')
 
     @yield('bottomcontent')
-    <!-- Load js libs only when the page is loaded. -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-    <script src="/js/modernizr.custom.72241.js"></script>
-    <!-- Custom template scripts -->
-    <script src="/js/magister.js"></script>
 
 </body>
 </html>
