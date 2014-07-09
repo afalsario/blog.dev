5a60cad592f77c5a60691ecc478a397246e55e07
Ashley <a_falsario@yahoo.com>
2014-07-09 11:19:18 -0500

    reformatted login page

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,28 +1,29 @@
 @extends('layouts.blog-master')
 
 @section('topscript')
-<link href="/css/journal-bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="/css/journal-bootstrap.min.css" rel="stylesheet" type="text/css">
 @stop
 
 @section('content')
-<div class="container">
-                <h5 class="text">Email</h5>
-                <div>
-                    {{ Form::open(array('action' => 'HomeController@doLogin'))}}
-                    {{ Form::text('email') }}
-                </div>
-                <h5 class="text">Password</h5>
-                <div>
-                    {{ Form::password('password') }}
-                    <div class="checkbox">
-                    <label>
-                        <input type="checkbox"> Checkbox
-                    </label>
-                    </div>
-                </div>
-            <div class="form-group">
-                {{ Form::submit('Log In', ['class' => 'btn btn-primary']) }}
+    <div class="center">
+        <h2 class="subtitle">Log In</h2>
+        <h5 class="text">Email</h5>
+        <div>
+            {{ Form::open(array('action' => 'HomeController@doLogin'))}}
+            {{ Form::text('email') }}
+        </div>
+        <h5 class="text">Password</h5>
+        <div>
+            {{ Form::password('password') }}
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox">Remember Me
+                </label>
             </div>
-            {{ Form::close() }}
-</div>
+        </div>
+        <div class="form-group">
+            {{ Form::submit('Log In', ['class' => 'btn btn-primary']) }}
+        </div>
+        {{ Form::close() }}
+    </div>
 @stop
