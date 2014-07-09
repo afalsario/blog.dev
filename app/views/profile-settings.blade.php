416b3b837de66500d1518b031ffdbcc0955a5c1e
Ashley <a_falsario@yahoo.com>
2014-07-09 15:32:37 -0500

    initial commit

---
+++ app/views/profile-settings.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.blog-master')
+
+@section('content')
+    <div class="container">
+        <h2 class="subtitle">Profile Info</h2>
+        First Name: {{{ Auth::user()->first_name }}}
+        <br>
+        Last Name: {{{ Auth::user()->last_name }}}
+        <br>
+        Email: {{{ Auth::user()->email }}}
+        <br>
+    </div>
+
+<!-- {{ Form::text('first_name') }}<br>
+    {{ $errors->first('first_name', '<span class="help-block">:message</span></br>') }}
+       {{ Form::text('last_name') }}<br>
+    {{ $errors->first('last_name', '<span class="help-block">:message</span></br>') }}
+    {{ Form::submit('Update', ['class' => 'btn btn-md btn-primary']) }}
+    {{ Form::close() }} <br> -->
+@stop
