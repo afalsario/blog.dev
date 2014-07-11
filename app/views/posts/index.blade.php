7710b72cf8e3437382fa137431c780f91240f9d4
Ashley <a_falsario@yahoo.com>
2014-07-11 11:16:36 -0500

    updating links to use the slug

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -11,13 +11,13 @@
 
         @foreach ($posts as $post)
             <div class="blog-post">
-                <h2 class="lead"><a href="{{ action('PostsController@show', $post->id) }}">{{{ $post->title }}}</a></h2>
+                <h2 class="lead"><a href="{{ action('PostsController@show', $post->slug) }}">{{{ $post->title }}}</a></h2>
                 <p>by <a href="#">{{{ $post->user->first_name . '  ' . $post->user->last_name }}}</a></p>
                     <hr>
                 <p><span class="glyphicon glyphicon-time"></span> {{{ $post->updated_at->format('F jS, Y g:i A') }}}</p>
                     <hr>
                 <p>{{{ $post->preview() }}}</p>
-                <a class="btn btn-primary" href="{{ action('PostsController@show', $post->id) }}">
+                <a class="btn btn-primary" href="{{ action('PostsController@show', $post->slug) }}">
                     Read More
                     <span class="glyphicon glyphicon-chevron-right"></span>
                 </a>
