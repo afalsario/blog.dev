e9c077ead6eb0ca8ab2b90e205b7473ab9b02833
Ashley <a_falsario@yahoo.com>
2014-07-11 09:24:38 -0500

    editing the profile view for editing info

--- app/views/profile-settings.blade.php
+++ app/views/profile-settings.blade.php
@@ -1,20 +1,26 @@
 @extends('layouts.blog-master')
 
+@section('topscript')
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+@stop
+
 @section('content')
-    <div class="center">
+
+    <div class="container">
         <h2 class="subtitle">Profile Info</h2>
-        First Name: {{{ Auth::user()->first_name }}}
+        {{ Form::label('first_name', 'First Name') }}<br>
+        {{ Form::text('first_name', Auth::user()->first_name) }}<br>
+        {{ $errors->first('first_name', '<span class="help-block">:message</span></br>') }}
+        {{ Form::label('last_name', 'Last Name') }}<br>
+        {{ Form::text('last_name', Auth::user()->last_name) }}<br>
+        {{ $errors->first('last_name', '<span class="help-block">:message</span></br>') }}
+        {{ Form::label('email', 'Email') }}<br>
+        {{ Form::text('email', Auth::user()->email) }}<br>
+        {{ $errors->first('email', '<span class="help-block">:message</span></br>') }}
         <br>
-        Last Name: {{{ Auth::user()->last_name }}}
-        <br>
-        Email: {{{ Auth::user()->email }}}
-        <br>
+        {{ Form::submit('Update', ['class' => 'btn btn-md btn-primary']) }}
+        {{ Form::close() }} <br>
     </div>
 
-<!-- {{ Form::text('first_name') }}<br>
-    {{ $errors->first('first_name', '<span class="help-block">:message</span></br>') }}
-       {{ Form::text('last_name') }}<br>
-    {{ $errors->first('last_name', '<span class="help-block">:message</span></br>') }}
-    {{ Form::submit('Update', ['class' => 'btn btn-md btn-primary']) }}
-    {{ Form::close() }} <br> -->
 @stop
